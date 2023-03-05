afe26979478a93d80d0250ada254acff681f2b99
celebrater <tkgm45nbchk@gmail.com>
2023-03-05 22:38:56 +0900

    moved the cursor in CeVIO, but not correct

--- autoCevio.ps1
+++ autoCevio.ps1
@@ -11,14 +11,31 @@
 [int[]] $chordsArray = @(0, 0, 7)
 [int[]] $notesArray = @(2, 4, 8, 16) # 16 == 1/16, 8 == 1/8
 [bool] $chordMode = $FALSE
+[int] $endToStart = 3 # the distance from the click end point to start
 # [int] $shortestNote = 16 
 # [int] $longestNote = 2
 # [int] $notes = $maxBar * $shortestNote
 # [int] $xGap = ($endX-$startX)/$notes
 # [int] $yGap = ($endY-$startY)/$fullLines
-[int] $interval = 1 # msec 
+[int] $interval = 3 # msec 
 [int] $tempo = 120
 [int] $times = 1
+
+# declare .NET Framework
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+# declare Windows API
+$signature=@'
+[DllImport("user32.dll",CharSet=CharSet.Auto,CallingConvention=CallingConvention.StdCall)]
+public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+'@
+$SendMouseClick = Add-Type -memberDefinition $signature -name "Win32MouseEventNew" -namespace Win32Functions -passThru
+
+# back to the previous window
+# ALT + TAB (move to the previous window)
+[System.Windows.Forms.SendKeys]::SendWait("%{TAB}")
+Start-Sleep -m 3000
 
 class CevioNote{
     [int] $startX = 0
@@ -26,15 +43,56 @@ class CevioNote{
     [int] $width = 33
     [string] $char = 'a'
 
-    [string] init($x, $y, $w, $ch){
+    init($x, $y, $w, $ch){
         $this.startX = $x
         $this.startY = $y
         $this.width = $w
         $this.char = $ch
         Write-Host $this.startX, $this.startY, $this.width, $this.char
-        return ''
+    }
+
+    # ste == start to end, ets == end to start
+    # writeNote($ets, $ms, $SendMouseClick){
+    #     [int] $ste = $this.width - $ets
+    #     [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($this.startX, $this.startY)
+    #     Start-Sleep -m $ms
+    #     $SendMouseClick::mouse_event(0x0002, 0, 0, 0, 0);
+    #     Start-Sleep -m $ms
+    #     [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($this.startX + $ste, $this.startY)
+    #     Start-Sleep -m $ms
+    #     $SendMouseClick::mouse_event(0x0004, 0, 0, 0, 0);
+    #     Start-Sleep -m $ms
+    #     [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($this.startX + $this.width, $this.startY)
+    #     Start-Sleep -m $ms
+    # }
+}
+
+function varDump($array){
+    foreach($item in $array){
+        Write-Host $item
     }
 }
 
+function writeNote($x, $y, $w, $ets){
+    [int] $end = $x + $ste
+    [int] $ste = $w - $ets
+    [int] $next = $x + $w
+    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
+    Start-Sleep -m $interval
+    $SendMouseClick::mouse_event(0x0002, 0, 0, 0, 0);
+    Start-Sleep -m $interval
+    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($end, $y)
+    Start-Sleep -m $interval
+    $SendMouseClick::mouse_event(0x0004, 0, 0, 0, 0);
+    Start-Sleep -m $interval
+    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($next, $y)
+    Start-Sleep -m $interval
+    
+    [int[]] $tempArray = @($end, $ste, $next)
+    varDump $tempArray
+}
+
 $notesObject = New-Object CevioNote
-$notesObject.init(3, 3, 35, 'i')
+$notesObject.init($startX, $startY, $barWidth/16, 'i')
+# $notesObject.writeNote($startX, $startY, $barWidth/16, 'i')
+writeNote $startX $startY $barWidth $endToStart
