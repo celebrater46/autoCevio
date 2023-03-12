7f1329e85a4575b8776c73386494461b411d24af
celebrater <tkgm45nbchk@gmail.com>
2023-03-12 22:35:08 +0900

    created classes successfully

--- classes/CevioNote.ps1
+++ classes/CevioNote.ps1
@@ -1,17 +1,22 @@
+[int] $x = $args[0]
+[int] $y = $args[1]
+[int] $width = $args[2]
+[string] $char = $args[3]
+
 class CevioNote{
     [int] $x = 960
     [int] $y = 540
     [int] $width = 96
     [string] $char = '‚ '
 
-    init(){
-        $this.x = $args[0]
-        $this.y = $args[1]
-        $this.width = $args[2]
-        $this.char = $args[3]
+    CevioNote($x, $y, $width, $char){
+        $this.x = $x
+        $this.y = $y
+        $this.width = $width
+        $this.char = $char
         Write-Host $this.x, $this.y, $this.width, $this.char
     }
 }
 
-$cn = [CevioNote]::new()
+$cn = [CevioNote]::new($x, $y, $width, $char)
 return $cn
